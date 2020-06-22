013f51a6a1984e7f66effcb1891ae2008d1e34f8
Sean <sean.chua.2011@sis.smu.edu.sg>
2020-06-22 23:18:06 +0800

    completed week 2

--- chapter2/phonebook/App.js
+++ chapter2/phonebook/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import personService from './services/persons'
 
 const Filter = ({ nameFilter, setNameFilter }) => {
 
@@ -19,28 +20,56 @@ const Filter = ({ nameFilter, setNameFilter }) => {
   )
 }
 
-const PersonForm = ({ persons, setPersons, newName, setNewName, newNumber, setNewNumber }) => {
+const PersonForm = ({ persons, setPersons, newName, setNewName, newNumber, setNewNumber, setMessage }) => {
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const addName = (event) => {
     event.preventDefault()
     const names = persons.map(person => person.name)
-    console.log("names", names)
+    // console.log("names", names)
+    const newPerson = { name: newName, number: newNumber }
+    const newMessage = () => {
+      setMessage(`Added ${newName}`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
 
     if (names.includes(newName)) {
       console.log(`${newName} is already added to phonebook`)
-      alert(`${newName} is already added to phonebook`)
+      // alert(`${newName} is already added to phonebook`)
+
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const oldPerson = persons.find(person => person.name === newName)
+        // const changedPerson = {...newPerson, id: oldPerson.id} //no need to add in id
+        personService.update(oldPerson.id, newPerson)
+          .then(response => {
+            setPersons(persons.map(person => person.id !== oldPerson.id ? person : newPerson))
+            setNewName('')
+            setNewNumber('')
+            newMessage()
+          })
+      }
+
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }))
+      // setPersons(persons.concat({ name: newName, number: newNumber }))
+      personService.create(newPerson)
+        .then(response => {
+          console.log(response)
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+          newMessage()
+        })
     }
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
@@ -61,17 +90,58 @@ const PersonForm = ({ persons, setPersons, newName, setNewName, newNumber, setNe
   )
 }
 
-const Persons = ({ persons, nameFilter }) => {
+const Persons = ({ persons, nameFilter, setPersons, setMessage, setMessageType }) => {
   const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(nameFilter.toLowerCase()))
 
   return (
     <div>
-      {filteredPersons.map(person => <Person key={person.name} person={person} />)}
+      {filteredPersons.map(person => <Person key={person.name} person={person} persons={persons} setPersons={setPersons} setMessage={setMessage} setMessageType={setMessageType} />)}
     </div>
   )
 }
 
-const Person = ({ person }) => <div>{person.name} {person.number}</div>
+const Person = ({ person, persons, setPersons, setMessage, setMessageType }) => {
+
+  const deleteId = person.id
+
+  const clickHandler = (event) => {
+    event.preventDefault()
+    if (window.confirm(`Delete ${person.name} ?`)) {
+      personService.remove(deleteId)
+        .then(response => {
+          setPersons(persons.filter(person => person.id !== deleteId))
+          // console.log(response.data) //empty
+        })
+        .catch(error => {
+          setMessageType('error')
+          setMessage(`Information of ${person.name} has aleady been removed from server`)
+          setTimeout(() => {
+            setMessage(null)
+            setMessageType(null)
+          }, 5000)
+        })
+    }
+  }
+
+  return (
+    <div>{person.name} {person.number} <button type="submit" onClick={clickHandler}>delete</button></div>
+  )
+}
+
+const Notification = ({ message, messageType }) => {
+  if (message === null) {
+    return null
+  }
+
+  if (messageType==='error'){
+    return <div className='messageError'>{message}</div>
+  } else {
+    return <div className='message'>{message}</div>
+  }
+
+  
+
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -83,26 +153,28 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
+  const [message, setMessage] = useState(null)
+  const [messageType, setMessageType] = useState(null)
 
   useEffect(() => {
     console.log('get persons')
-    axios
-      .get('http://localhost:3001/persons')
+    personService.getAll()
       .then(response => {
-        console.log('promise fulfilled')
+        console.log('persons promise fulfilled')
         setPersons(response.data)
       })
   }, [])
-  console.log('render', persons.length, 'persons')
+  // console.log('render', persons.length, 'persons')
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} messageType={messageType} />
       <Filter nameFilter={nameFilter} setNameFilter={setNameFilter} />
       <h2>add a new</h2>
-      <PersonForm persons={persons} setPersons={setPersons} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
+      <PersonForm persons={persons} setPersons={setPersons} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} setMessage={setMessage} />
       <h2>Numbers</h2>
-      <Persons persons={persons} nameFilter={nameFilter} />
+      <Persons persons={persons} nameFilter={nameFilter} setPersons={setPersons} setMessage={setMessage} setMessageType={setMessageType} />
     </div>
   )
 }
